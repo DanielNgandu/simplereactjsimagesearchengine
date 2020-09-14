3f98b3fc856857fdd652d64df0fd27979eeb158a
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-09-14 06:06:59 +0200

    Add unsplash api

--- src/App.js
+++ src/App.js
@@ -4,25 +4,50 @@ import React from "react";
 //import React Dom
 import ReactDOM from "react-dom";
 
+//import 3rd party libs
+import axios from "axios";
 
 //import custom components here
-import  SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar";
 
 import ImageList from "./ImageList";
 
-class App extends React.Component{
-    render(){
-        return (
-            <div>
-                <form className="ui form">
-                    <div className="field fluid">
-                <SearchBar/>
-                    </div>
-                </form>
-                <ImageList/>
-            </div>
-        );
-    }
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (searchkeyword) => {
+    console.clear();
+
+    console.log(searchkeyword);
+    //pass searched word to axios
+    const response = await axios.get(
+      "https://api.unsplash.com//search/photos",
+      {
+        params: { query: searchkeyword },
+        headers: {
+          Authorization:
+            "Client-ID zToMPnfaitCW3AK-jp8A91XMKekYTL0snz2rj50b-XM",
+        },
+        //   console.warn(resp.data);
+      }
+    );
+
+    //print response
+    console.log(response.data.results);
+
+    //set state
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmitProp={this.onSearchSubmit} />
+        <p>Found: {this.state.images.length} images.</p>
+        <ImageList />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -8,14 +8,36 @@ import ReactDOM from "react-dom";
 
 //class based component
 class SearchBar extends React.Component{
+    //constructor
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { searchkeyword: ''}
+    //     this.onFormSubmit = this.onFormSubmit.bind(this)
+    //
+    //
+    // }
+    state = {searchkeyword:''};
+
+    onFormSubmit =(event)=> {
+        event.preventDefault();
+        // console.log(this.state.searchkeyword);
+        //get props
+        this.props.onSubmitProp(this.state.searchkeyword);
+
+    }
+
     render() {
         return(
             <div>
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field fluid">
                 <label>Search</label>
                 <div className="ui fluid icon input loading">
-                    <input type="text" placeholder="Search..."/>
+                    <input type="text" placeholder="Search..." value={this.state.searchkeyword} onChange={event => this.setState({searchkeyword: event.target.value})}/>
                         <i className="search icon"></i>
                 </div>
+                    </div>
+                </form>
                 <div className="ui horizontal divider">
                     Search Results:
                 </div>
